8cc9953e88d16e757b5030afe1e99c9d00fbd873
chenyule <chenyule-o2o@bestpay.com.cn>
2019-08-16 18:17:24 +0800

    深拷贝
    简单深拷贝和稍复杂拷贝
    1、对正则的处理
    2、对原型的处理

--- utils.common.js
+++ utils.common.js
@@ -25,12 +25,96 @@ export const debounce = (fn, delay) => {
  * @param {*} delay 
  */
 export const throttle = (fn, delay = 500) => {
-  let falg = true;
+  let flag = true;
   return (...args) => {
-    if (!falg) return
-    falg = false
+    if (!flag) return
+    flag = false
     setTimeout(() => {
       fn.apply(this, args)
     }, delay)
   }
+}
+
+/**
+ * 深拷贝：deepClone
+ * newObj 简单的拷贝
+ * 局限：1、无法实现对函数 、RegExp等特殊对象的克隆
+ *      2、会抛弃对象的constructor,所有的构造函数会指向Object
+ *      3、对象有循环引用,会报错
+ * clone
+ * 局限：1、一些特殊情况没有处理: 例如Buffer对象、Promise、Set、Map
+ *      2、另外对于确保没有循环引用的对象，我们可以省去对循环引用的特殊处理，因为这很消耗时间
+ * @param {*} parent object 需要进行克隆的对象
+ * @return {*} child 克隆后的对象
+ */
+export const newObj = JSON.parse(JSON.stringify(oldObj))
+
+export const clone = (parent) => {
+  const isType = (obj, type) => {
+    if (typeof obj !== "object") return false
+    const typeString = Object.prototype.toString.call(obj)
+    let flag;
+    switch (type) {
+      case "Array":
+        flag = typeString === "[object,Array]"
+        break;
+      case "RegExp":
+        flag = typeString === "[object,RegExp]"
+        break;
+      case "Date":
+        flag = typeString === "[object,Date]"
+      default:
+        flag = false
+    }
+    return flag
+  }
+
+  const getRegExp = (re) => {
+    var flags = "";
+    if (re.global) flags += 'g';
+    if (re.ignoreCase) flags += 'i';
+    if (re.multiline) flags += 'm';
+    return flags;
+  }
+
+  // 维护两个存储循环引用的数组
+  const parents = []
+  const children = []
+
+  const _clone = (parent) => {
+    if (parent === null) return null;
+    if (typeof parent !== 'object') return parent;
+    let child,proto
+
+    // 数组 正则 Date 单独处理->判断类型
+    if (isType(parent, "Array")) {
+      // 数组单独处理
+      child = []
+    } else if (isType(parent, "RegExp")) {
+      child = new RegExp(parent.souce, getRegExp(parent))
+      if (parent.lastIndex) child.lastIndex = parent.lastIndex
+    }else if (isType(parent,"Date")){
+      child = new Date(parent.getTime())
+    }else{
+      // 处理对象原型
+      proto = Object.getPrototypeOf(parent)
+      // 利用Object.create切断原型链
+      child = Object.create(proto)
+    }
+
+    const Index = parents.indexOf(parent)
+
+    if (Index != -1) {
+      return children[Index]
+    }
+
+    parents.push(parent)
+    children.push(child)
+
+    for (let i in parent) {
+      child[i] = _clone(parent[i])
+    }
+    return child
+  }
+  return _clone(parent)
 }
